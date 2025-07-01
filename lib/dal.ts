8e10f963f5e6ee22f4ce41986215e3a16096b018
lmohade3l <mohade3.2710@gmail.com>
2025-07-01 19:29:19 +0330

    feat: dal: getCurrentUser function

--- lib/dal.ts
+++ lib/dal.ts
@@ -4,3 +4,20 @@ import { eq } from 'drizzle-orm'
 import { cache } from 'react'
 import { issues, users } from '@/db/schema'
 import { mockDelay } from './utils'
+
+export const getCurrentUser = async () => {
+    const session = await getSession()
+    if (!session) return null
+  
+    try {
+      const result = await db
+        .select()
+        .from(users)
+        .where(eq(users.id, session.userId))
+  
+      return result[0] || null
+    } catch (error) {
+      console.error('Error getting user by ID:', error)
+      return null
+    }
+  }
